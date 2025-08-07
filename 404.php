955738ede618c5bd2af61acf8ff59e154fbc580d
gaba-work <malkocmax@gmail.com>
2025-08-07 11:35:04 +0300

    upd: some

--- 404.php
+++ 404.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * The template for displaying 404 pages (not found)
  *
@@ -11,27 +12,27 @@ get_header();
 ?>
 <meta http-equiv="refresh" content="5;url=/" />
 <main id="primary" class="site-main">
-    <section class="error-404 not-found" style="padding-top: 30vh; padding-bottom: 30vh;">
-        <header class="page-header">
-            <h1 class="page-title" style="text-align:center; margin-top:10vh;">404 - La pagina che stavi cercando non
-                esiste. <br>
-                <progress value="0" max="4" id="progressBar"></progress>
-            </h1>
-        </header><!-- .page-header -->
-        <div class="page-content" style="text-align:center">
-            <p>La pagina richiesta non è stata trovata, <br> tra pochi secondi sarai rediretto all'home page</p>
-        </div><!-- .page-content -->
-    </section><!-- .error-404 -->
+  <section class="error-404 not-found" style="padding-top: 30vh; padding-bottom: 30vh;">
+    <header class="page-header">
+      <h1 class="page-title" style="text-align:center; margin-top:10vh;">404 - La pagina che stavi cercando non
+        esiste. <br>
+        <progress value="0" max="4" id="progressBar"></progress>
+      </h1>
+    </header><!-- .page-header -->
+    <div class="page-content" style="text-align:center">
+      <p>La pagina richiesta non è stata trovata, <br> tra pochi secondi sarai rediretto all'home page</p>
+    </div><!-- .page-content -->
+  </section><!-- .error-404 -->
 </main><!-- #main -->
 <script>
-var timeleft = 4;
-var downloadTimer = setInterval(function() {
+  var timeleft = 4;
+  var downloadTimer = setInterval(function() {
     if (timeleft <= 0) {
-        clearInterval(downloadTimer);
+      clearInterval(downloadTimer);
     }
     document.getElementById("progressBar").value = 4 - timeleft;
     timeleft -= 1;
-}, 1000);
+  }, 1000);
 </script>
 <?php
 get_footer();
--- front-page.php
+++ front-page.php
@@ -5,9 +5,13 @@
  */
 get_header();
 ?>
-<?php get_template_part('template-parts/home/home-intro');?>
-<?php get_template_part('template-parts/home/what-we-do');?>
-<?php get_template_part('template-parts/home/why-us');?>
-<?php get_template_part('template-parts/home/transporting');?>
-<?php get_template_part('template-parts/home/reviews');?>
+
+<main>
+  <?php get_template_part('template-parts/home/home-intro'); ?>
+  <?php get_template_part('template-parts/home/what-we-do'); ?>
+  <?php get_template_part('template-parts/home/why-us'); ?>
+  <?php get_template_part('template-parts/home/transporting'); ?>
+  <?php get_template_part('template-parts/general/reviews'); ?>
+</main>
+
 <?php get_footer(); ?>
