4874b3eaf2352bf9171c39cd101b68f68b965514
gaba-linux-home <malkocmax@gmail.com>
2025-08-13 14:57:24 +0300

    feat: finished blog

--- front-page.php
+++ front-page.php
@@ -10,7 +10,8 @@ get_header();
 <?php get_template_part('template-parts/home/what-we-do'); ?>
 <?php get_template_part('template-parts/home/why-us'); ?>
 <?php get_template_part('template-parts/home/transporting'); ?>
-<?php get_template_part('template-parts/general/reviews'); ?>
+<?php get_template_part('template-parts/globals/reviews'); ?>
 <?php get_template_part("template-parts/home/choose-us"); ?>
-<?php get_template_part("template-parts/home/team"); ?>
+<?php get_template_part("template-parts/globals/team"); ?>
+<?php get_template_part("template-parts/home/blog"); ?>
 <?php get_footer(); ?>
---
+++ template-parts/home/blog.php
@@ -0,0 +1,45 @@
+<?php
+$blog = get_field('blog');
+$tag = $blog['tag'];
+$title = $blog['title'];
+$items = $blog['items'];
+$button_text = $blog['button_text'];
+$button_url = $blog['button_url'];
+?>
+
+<section class="blog">
+  <div class="blog__tag tag"><?php echo $tag; ?></div>
+  <h2 class="blog__title title"><?php echo $title; ?></h2>
+  <div class="blog__body">
+    <?php foreach ($items as $item): ?>
+      <?php
+      $image = $item['image'];
+      $date = $item['date'];
+      $item_title = $item['item_title'];
+      $description = $item['description'];
+      $advantages = $item['advantages'];
+      ?>
+      <div class="blog__news">
+        <img src="<?php echo $image['url']; ?>" width="453" height="308" class="blog__img" alt="<?php echo $image['alt']; ?>" />
+        <!-- <img src="./../src/img/home-page/blog1.jpg" width="453" height="308" alt="" class="blog__img"> -->
+        <div class="blog__calendar">
+          <?php get_template_part('template-parts/icons/icon-calendar'); ?>
+          <div class="blog__date"><?php echo $date; ?></div>
+        </div>
+        <div class="blog__info">
+          <h3 class="blog__subtitle subtitle"><?php echo $item_title; ?></h3>
+          <p class="blog__desk"><?php echo $description; ?></p>
+          <ul class="blog__list">
+            <?php foreach ($advantages as $item): ?>
+              <?php
+              $label = $item['label'];
+              ?>
+              <li class="blog__item"><?php echo $label; ?></li>
+            <?php endforeach; ?>
+          </ul>
+        </div>
+      </div>
+    <?php endforeach; ?>
+  </div>
+  <a class="blog__button button button--blue" href="<?php echo $button_url; ?>" target="_blank"><?php echo $button_text; ?></a>
+</section>
