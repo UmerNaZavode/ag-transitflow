6161dc81746e599520f2448b86b90714f9244806
gaba-work <malkocmax@gmail.com>
2025-08-18 17:01:44 +0300

    feat: finished project details

--- page-warehouse-transportation.php
+++ page-warehouse-transportation.php
@@ -6,4 +6,5 @@ get_header();
 ?>
 
 <?php get_template_part("template-parts/globals/page-intro"); ?>
+<?php get_template_part("template-parts/warehouse-transportation/project-details"); ?>
 <?php get_footer(); ?>
--- template-parts/warehouse-transportation/project-details.php
+++ template-parts/warehouse-transportation/project-details.php
@@ -1,2 +1,29 @@
-<div class="project-details">
-</div>
+<?php
+$project_details = get_field('project_details');
+$image = $project_details['image'];
+$title = $project_details['title'];
+$items = $project_details['items'];
+?>
+
+<section class="project-details">
+  <div class="project-details__wrapper container">
+    <div class="project-details__left">
+      <img src="<?php echo $image['url']; ?>" class="project-details__img" alt="<?php echo $image['alt']; ?>" />
+    </div>
+    <div class="project-details__right">
+      <h3 class="project-details__title"><?php echo $title; ?></h3>
+      <div class="project-details__inner">
+        <?php foreach ($items as $item): ?>
+          <?php
+          $label = $item['label'];
+          $key = $item['key'];
+          ?>
+          <div class="project-details__list">
+            <div class="project-details__label"><?php echo $label; ?></div>
+            <div class="project-details__key"><?php echo $key; ?></div>
+          </div>
+        <?php endforeach; ?>
+      </div>
+    </div>
+  </div>
+</section>
