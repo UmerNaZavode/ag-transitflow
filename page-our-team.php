590e57797de4594935ae588e95f65c2b90faed17
gaba-work <malkocmax@gmail.com>
2025-08-18 18:59:37 +0300

    feat: started blog

---
+++ page-our-team.php
@@ -0,0 +1,10 @@
+<?php
+/*
+ *
+ */
+get_header();
+?>
+
+<?php get_template_part("template-parts/globals/page-intro"); ?>
+<?php get_template_part("template-parts/globals/team"); ?>
+<?php get_footer(); ?>
