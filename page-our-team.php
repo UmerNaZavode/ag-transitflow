5abefa763f71469d12c0485a93102548f2bbf7d4
gaba-work <malkocmax@gmail.com>
2025-08-20 13:59:29 +0300

    upd: our-team page fixes

--- page-our-team.php
+++ page-our-team.php
@@ -8,4 +8,5 @@ get_header();
 <?php get_template_part("template-parts/globals/page-intro"); ?>
 <?php get_template_part("template-parts/globals/team"); ?>
 <?php get_template_part("template-parts/our-team/team-about"); ?>
+<?php get_template_part("template-parts/globals/companies"); ?>
 <?php get_footer(); ?>
