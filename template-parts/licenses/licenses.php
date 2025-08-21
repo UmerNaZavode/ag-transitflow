1fd6c1f5e2cfedea0cadfabd8bca22e6ebdf219d
gaba-work <malkocmax@gmail.com>
2025-08-21 11:19:43 +0300

    feat: finished licenses page

--- template-parts/licenses/licenses.php
+++ template-parts/licenses/licenses.php
@@ -1,4 +1,18 @@
-<?php 
- ?>
-<div class="licenses">
-</div>
+<?php
+$licenses = get_field('licenses');
+$items = $licenses['items'];
+?>
+<section class="licenses container">
+  <?php foreach ($items as $item): ?>
+    <?php
+    $title = $item['title'];
+    $info = $item['info'];
+    ?>
+    <div class="licenses__wrapper">
+      <h3 class="licenses__title"><?php echo $title; ?></h3>
+      <div class="licenses__content">
+        <div class="licenses__info"><?php echo $info; ?></div>
+      </div>
+    </div>
+  <?php endforeach; ?>
+</section>
