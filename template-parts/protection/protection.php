8bc4081be5e600763723e03a91fea4d3048d48c8
gaba-work <malkocmax@gmail.com>
2025-08-21 11:58:35 +0300

    feat: protection page finished

--- template-parts/protection/protection.php
+++ template-parts/protection/protection.php
@@ -1,4 +1,10 @@
-<?php 
- ?>
-<div class="protection">
-</div>
+<?php
+?>
+<section class="protection container">
+  <div class="protection__wrapper">
+    <h2 class="protection__title">Enter Password</h2>
+    <div class="protection__form form">
+      <?php echo do_shortcode('[contact-form-7 id="b3abd48" title="Protection"]'); ?>
+    </div>
+  </div>
+</section>
